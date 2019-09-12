b680678318e6c811b0370bc729b3311ba4e6fa7d
Erik Östlind <54645344+indu898@users.noreply.github.com>
2019-09-12 15:07:16 +0200

    Add files via upload
    chchchchchc changes

--- projektarbete/1.0 Färdiga saker/fleximg.js
+++ projektarbete/1.0 Färdiga saker/fleximg.js
@@ -1,8 +1,8 @@
 // Loads and writes  OR deletes two images with below properties 
 // if the width property is or is not true
 
-function addRemoveImage(x) {
-    if (x.matches) {
+function addRemoveImage(y) {
+    if (y.matches) {
         let myImage3 = new Image
         myImage3.src = "images/flexboy.png"
         document.id = body2.appendChild(myImage3)
@@ -14,10 +14,21 @@ function addRemoveImage(x) {
         myImage4.setAttribute("id", "image2")
 
     } else {
-        try { document.getElementById("image").remove(); } catch (e) { }
-        try { document.getElementById("image2").remove(); } catch (e) { }
+        // try { document.getElementById("image").remove(); } catch (e) { }
+        // try { document.getElementById("image2").remove(); } catch (e) { }
+        const image1 = document.getElementById("image");
+        const image2 = document.getElementById("image2");
+
+        if (image1) {
+            image1.remove();
+        }
+
+        if (image2) {
+            image2.remove();
+        }
     }
 }
-var x = window.matchMedia("(min-width: 992px)")
-addRemoveImage(x) // Call listener function at run time
-x.addListener(addRemoveImage) // Attach listener function on state changes
+
+var mediaQueryList = window.matchMedia("(min-width: 992px)")
+addRemoveImage(mediaQueryList) // Call listener function at run time
+mediaQueryList.addListener(addRemoveImage) // Attach listener function on state changes
--- projektarbete/1.0 Färdiga saker/indeximg.js
+++ projektarbete/1.0 Färdiga saker/indeximg.js
@@ -1,8 +1,8 @@
 // Loads and writes  OR deletes two images with below properties 
 // if the width property is or is not true
 
-function addRemoveImage(x) {
-    if (x.matches) {
+function addRemoveImage(y) {
+    if (y.matches) {
         let myImage = new Image
         myImage.src = "images/float5.png"
         document.id = body1.appendChild(myImage)
@@ -14,10 +14,20 @@ function addRemoveImage(x) {
         myImage2.setAttribute("id", "image2")
 
     } else {
-        try { document.getElementById("image").remove(); } catch (e) { }
-        try { document.getElementById("image2").remove(); } catch (e) { }
+        // try { document.getElementById("image").remove(); } catch (e) { }
+        // try { document.getElementById("image2").remove(); } catch (e) { }
+        const image1 = document.getElementById("image");
+        const image2 = document.getElementById("image2");
+
+        if (image1) {
+            image1.remove();
+        }
+
+        if (image2) {
+            image2.remove();
+        }
     }
 }
-var x = window.matchMedia("(min-width: 992px)")
-addRemoveImage(x) // Call listener function at run time
-x.addListener(addRemoveImage) // Attach listener function on state changes
+var mediaQueryList = window.matchMedia("(min-width: 992px)")
+addRemoveImage(mediaQueryList) // Call listener function at run time
+mediaQueryList.addListener(addRemoveImage) // Attach listener function on state changes
